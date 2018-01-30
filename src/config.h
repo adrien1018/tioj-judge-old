01df802331a3621197c9199801f9d38e33d1fbeb
adrien1018 <adrien1018@users.noreply.github.com>
2018-01-30 22:00:06 +0800

    Add configure file

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+
+#include <unordered_map>
+#include <string>
+
+extern const std::unordered_map<std::string, int> SyscallNum;
+extern const std::string BoxPath;
+
+#endif
--- syscall_list.h
+++
@@ -1,9 +0,0 @@
-#ifndef SYSCALL_LIST_H_INCLUDED
-#define SYSCALL_LIST_H_INCLUDED
-
-#include <unordered_map>
-#include <string>
-
-extern const std::unordered_map<std::string, int> syscall_num;
-
-#endif // SYSCALL_LIST_H_INCLUDED
